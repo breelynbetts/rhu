70a09a52b2ae4554cc5117777cc2914bb2562923
KeAnna Anglin <keannaanglin@KAs-MacBook-Pro.local>
2017-08-15 23:41:33 -0700

    working on adventure page

--- Scripts/adventure.js
+++ Scripts/adventure.js
@@ -1,7 +1,9 @@
 // Adventure JavaScript
+var map;
+
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.6687, lng: 7.20645},
           zoom: 13
         });
 
@@ -51,3 +53,51 @@ function initMap() {
           infowindow.open(map, marker);
         });
 }
+function onclick() {
+  // select from array
+  console.log($(this).find(':selected').val());
+  console.log($('#landmarks').val() !== $(this).find(':selected').val());
+  if ($(this).find(':selected').val() !== $('#landmarks').val()) {
+    console.log('helloooo');
+    return;
+  }
+  var array = [
+    new google.maps.LatLng(50.6687, 7.2064),
+    new google.maps.LatLng(50.7369, 7.1012),
+    new google.maps.LatLng(50.7334, 7.0995),
+    new google.maps.LatLng(50.7247, 7.0931),
+  ];
+  console.log(array);
+
+  var selection = array[Math.floor(Math.random() * array.length)]
+
+  // var map = new google.maps.Map(document.getElementById('map'), {
+  //   center: {lat: -33.8688, lng: 151.2195},
+  //   zoom: 13
+  // })
+  var marker = new google.maps.Marker({
+    map: map,
+    position: selection
+  });
+
+  marker.setPlace({
+    placeId: 'test',
+    location: selection
+  });
+  marker.setVisible(true);
+
+  //do something with that selection.  You would interact with the map
+  // $('.result').text('you clicked the button and we selected' + selection)
+};
+
+
+
+function setup() {
+  // when the elements that match #1 are clicked we call the function onclick
+  console.log('setting up...');
+  $('#dropdown').on('change', onclick);
+}
+
+// We put stuff in document.ready so that the javascript renders after the html.
+// this ensures that it can grab html elements. (ie the button and the map)
+$(document).ready(setup)
